62706776c7cf47fa0022355c83ee07384970e358
Zrital <Ztrital12580@gmail.com>
2024-04-02 17:57:13 +0545

    feat darkmode using contextapi

--- crashCourse-II/src/App.jsx
+++ crashCourse-II/src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { ThemeProvider } from "./context/theme";
+import { useEffect } from "react";
+import ThemeBtn from "./components/ThemeButton";
+import Card from "./components/Card";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [themeMode, setThemeMode] = useState("light");
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+
+  //toggle theme
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+        <div className="flex flex-wrap items-center min-h-screen">
+          <div className="w-full">
+            <div className="flex justify-end w-full max-w-sm mx-auto mb-4">
+              <ThemeBtn />
+            </div>
+
+            <div className="w-full max-w-sm mx-auto">
+              <Card />
+            </div>
+          </div>
+        </div>
+      </ThemeProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
