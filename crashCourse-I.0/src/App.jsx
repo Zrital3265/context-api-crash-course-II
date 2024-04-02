7e2ede54400c1361ab376a5aa764edfa7cc69c66
Zrital <Ztrital12580@gmail.com>
2024-04-02 09:18:46 +0545

    feat : totaling cart

--- crashCourse-I.0/src/App.jsx
+++ crashCourse-I.0/src/App.jsx
@@ -1,13 +1,15 @@
 import Items from "./components/Items";
 import "./App.css";
+import Cart from "./components/CartCheckout";
 
 const App = () => {
   return (
     <div>
-      <Items name="Lenovo Thinkpad69" price="$69" />
-      <Items name="Macbook Air Pro" price="$69" />
-      <Items name="HP victus 69" price="$69" />
-      <Items name="xiaomi yoga pad" price="$69" />
+      <Items name="Lenovo Thinkpad69" price={400000} />
+      <Items name="Macbook Air Pro" price={4500000} />
+      <Items name="HP victus 69" price={80000} />
+      <Items name="xiaomi yoga pad" price={100000} />
+      <Cart />
     </div>
   );
 };
